d5cf3e3470ca2a4fceb1f39c36e35e3f6c6acf7b
smruti-design <ranjan.sahoo26kumar@gmail.com>
2024-03-01 18:21:21 +0530

    Added some method for testing more feature

--- src/test/java/com/sdw/MXApplication/RestAssuredTests.java
+++ src/test/java/com/sdw/MXApplication/RestAssuredTests.java
@@ -1,5 +1,7 @@
 package com.sdw.MXApplication;
 
+import com.sdw.MXApplication.model.Address;
+import com.sdw.MXApplication.model.Employee;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RestAssuredTests {
@@ -26,5 +29,32 @@ public class RestAssuredTests {
                 .get()
                 .then()
                 .assertThat().body("name", Matchers.equalTo("Smruti"));
+    }
+
+    @Test
+    public void testGetEmployeeByID2(){
+        //Arrange
+        var employee = Employee.builder().id(1)
+                .email("smruti.digiworl@gmail.com")
+                .phone(1234567890)
+                .address(Address.builder()
+                        .city("Bhubaneswar")
+                        .country("India")
+                        .street("Sachivalaya Marg").build())
+                .name("Smruti")
+                .build();
+
+
+        //Act
+        var response = given().
+                baseUri(baseUrl)
+                .port(port)
+                .basePath("employee/1")
+                .get();
+
+        var responseEntity = response.body().as(Employee.class);
+
+        //Assert
+        assertThat(responseEntity).isEqualTo(employee) ;
     }
 }
